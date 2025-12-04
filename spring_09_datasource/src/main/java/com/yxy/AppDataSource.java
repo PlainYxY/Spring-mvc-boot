15b074f2089c571a63dfecbdf878540e78fc646c
YxY <1220459220@qq.com>
2025-12-04 21:52:21 +0800

    12.4

--- spring_09_datasource/src/main/java/com/yxy/AppDataSource.java
+++ spring_09_datasource/src/main/java/com/yxy/AppDataSource.java
@@ -4,16 +4,14 @@ import com.yxy.dao.BookDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import javax.sql.DataSource;
-
-public class App {
+public class AppDataSource {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        DataSource dataSource = (DataSource) ctx.getBean("dataSource");
-        System.out.println(dataSource);
+        // DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+        // System.out.println(dataSource);
 
-        // BookDao bookDao = (BookDao) ctx.getBean("bookDao");
-        // bookDao.save();
+        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+        bookDao.save();
 
     }
 }
