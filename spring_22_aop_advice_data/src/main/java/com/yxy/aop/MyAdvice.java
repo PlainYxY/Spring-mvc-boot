b389ef274ba7962821c976a16a6975235ae14c81
YxY <1220459220@qq.com>
2025-12-11 23:03:01 +0800

    12.11

--- spring_22_aop_advice_data/src/main/java/com/yxy/aop/MyAdvice.java
+++ spring_22_aop_advice_data/src/main/java/com/yxy/aop/MyAdvice.java
@@ -24,13 +24,16 @@ public class MyAdvice {
     }
 
     // @After("pt()")
+    ///知识点： 对于@Before或者@After注释，使用JoinPoint jp 获取调用原始方法的参数
     public void after(JoinPoint jp) {
         Object[] args = jp.getArgs();
         System.out.println(Arrays.toString(args));
         System.out.println("after advice");
     }
 
+
     // @Around("pt()")
+    ///知识点： 对于@Around注释，使用ProceedingJoinPoint pjp 获取调用原始方法的参数
     public Object aroundSelect(ProceedingJoinPoint pjp) throws Throwable {
         Object[] args = pjp.getArgs();
         System.out.println(Arrays.toString(args));
@@ -40,16 +43,26 @@ public class MyAdvice {
         /// 知识点：以上获取到通知数据后,还可以经过修改,然后通过.proceed(args),再传回切点方法
         Object proceed = pjp.proceed(args);
         return proceed;
+
+        // 用try-catch块可以拿到异常信息
+        // Object proceed = null;
+        // try {
+        //     proceed = pjp.proceed(args);
+        // } catch (Throwable t) {
+        //     throw new RuntimeException(t);
+        // }
+        // return proceed;
     }
 
-    // 这个注解用如下方式接受返回值
+    /// 这个注解用如下方式接受返回值
     @AfterReturning(value = "pt()",returning = "ref")
-    public void afterReturning(Object ref) {
+    // 如果 JoinPoint参数和其他参数一起出现在形参中，则它必须是第一个，不然会报错
+    public void afterReturning(JoinPoint jp, String ref) {
         System.out.println("afterReturning advice " + ref);
     }
 
-    // @AfterThrowing("pt()")
-    public void afterThrowing(JoinPoint joinPoint) {
-        System.out.println("afterThrowing advice");
+    // @AfterThrowing(value = "pt()",throwing = "t")
+    public void afterThrowing(Throwable t) {
+        System.out.println("afterThrowing advice" + t);
     }
 }
