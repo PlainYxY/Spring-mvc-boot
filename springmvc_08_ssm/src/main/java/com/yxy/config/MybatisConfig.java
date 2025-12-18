a93b879eccd4875b0cef654ab453c1e2a4cafbb4
YxY <1220459220@qq.com>
2025-12-18 18:12:03 +0800

    12.18

---
+++ springmvc_08_ssm/src/main/java/com/yxy/config/MybatisConfig.java
@@ -0,0 +1,25 @@
+package com.yxy.config;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class MybatisConfig {
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTypeAliasesPackage("com.yxy.domain");
+        return factoryBean;
+    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer msc = new MapperScannerConfigurer();
+        msc.setBasePackage("com.yxy.dao");
+        return msc;
+    }
+}
---
+++ springmvc_08_ssm/src/main/java/com/yxy/service/impl/BookServiceImpl.java
@@ -0,0 +1,39 @@
+package com.yxy.service.impl;
+
+import com.yxy.dao.BookDao;
+import com.yxy.domain.Book;
+import com.yxy.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    public boolean save(Book book) {
+        bookDao.save(book);
+        return true;
+    }
+
+    public boolean delete(Integer id) {
+        bookDao.delete(id);
+        return true;
+    }
+
+    public boolean update(Book book) {
+        bookDao.update(book);
+        return true;
+    }
+
+    public Book getById(Integer id) {
+        return bookDao.getById(id);
+    }
+
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
