dc8629fccd975859518aab38d0b9b67993f103fe
YxY <1220459220@qq.com>
2025-12-21 21:11:52 +0800

    12.21

--- springmvc_11_exception2/src/main/java/com/yxy/controller/ProjectExceptionAdvice.java
+++ springmvc_11_exception2/src/main/java/com/yxy/controller/ProjectExceptionAdvice.java
@@ -1,10 +1,28 @@
 package com.yxy.controller;
 
+import com.yxy.exception.BusinessException;
+import com.yxy.exception.SystemException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ProjectExceptionAdvice {
+
+    // 自定义的处理 系统异常
+    @ExceptionHandler(SystemException.class)
+    public Result doSystemException(SystemException ex) {
+        // 记录日志
+        // 发送消息给运维
+        // 发送邮件给开发人员（把ex对象返回给开发）
+        return new  Result(ex.getCode(), null, ex.getMessage());
+    }
+
+    // 自定义的处理 业务异常
+    @ExceptionHandler(BusinessException.class)
+    public Result doBusinessException(BusinessException ex) {
+        return new  Result(ex.getCode(), null, ex.getMessage());
+    }
+
 
 
     // 处理 系统未知的异常异常（通常返回友好消息（返回那种安抚客户的消息））
--- springmvc_11_exception2/src/main/java/com/yxy/service/impl/BookServiceImpl.java
+++ springmvc_11_exception2/src/main/java/com/yxy/service/impl/BookServiceImpl.java
@@ -3,6 +3,8 @@ package com.yxy.service.impl;
 import com.yxy.controller.Code;
 import com.yxy.dao.BookDao;
 import com.yxy.domain.Book;
+import com.yxy.exception.BusinessException;
+import com.yxy.exception.SystemException;
 import com.yxy.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,7 +33,21 @@ public class BookServiceImpl implements BookService {
     }
 
     public Book getById(Integer id) {
-        bookDao.getById(id);
+
+        // 模拟一个业务异常
+        if (id < 1){
+            throw new BusinessException(Code.BUSINESS_ERR,"请勿进行非法操作（这是一个业务异常）");
+        }
+
+        // 模拟一个系统异常
+        // 将可能出现的异常包装，转换为自定义异常
+        try{
+            int i = 1/0;
+        }catch (Exception e){
+            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时...",e);
+        }
+
+
         return bookDao.getById(id);
     }
 
