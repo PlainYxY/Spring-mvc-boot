2b8f5b9e599426d0fe6a13c2779a01408415a73d
YxY <1220459220@qq.com>
2025-12-22 21:00:08 +0800

    12.22

--- springmvc_12_page/src/main/java/com/yxy/service/impl/BookServiceImpl.java
+++ springmvc_12_page/src/main/java/com/yxy/service/impl/BookServiceImpl.java
@@ -31,18 +31,18 @@ public class BookServiceImpl implements BookService {
 
     public Book getById(Integer id) {
 
-        // 模拟一个业务异常
-        if (id < 1){
+        // // 模拟一个业务异常
+        if (id == 1){
             throw new BusinessException(Code.BUSINESS_ERR,"请勿进行非法操作（这是一个业务异常）");
         }
 
-        // 模拟一个系统异常
-        // 将可能出现的异常包装，转换为自定义异常
-        try{
-            int i = 1/0;
-        }catch (Exception e){
-            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时...",e);
-        }
+        // // 模拟一个系统异常
+        // // 将可能出现的异常包装，转换为自定义异常
+        // try{
+        //     int i = 1/0;
+        // }catch (Exception e){
+        //     throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时...",e);
+        // }
 
 
         return bookDao.getById(id);
