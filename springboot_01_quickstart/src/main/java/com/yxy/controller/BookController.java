6a134e640e48388bc7bb1ee128d8d4f19f580ec4
YxY <1220459220@qq.com>
2025-12-23 21:37:40 +0800

    12.23

---
+++ springboot_01_quickstart/src/main/java/com/yxy/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.yxy.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable String id) {
+        System.out.println("id =>" + id);
+        return "hello, springboot";
+    }
+}
