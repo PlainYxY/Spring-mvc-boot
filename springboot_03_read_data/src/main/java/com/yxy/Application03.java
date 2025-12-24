e6c7ded16b729d721f7adba49658f9d68ce6ebc5
YxY <1220459220@qq.com>
2025-12-24 21:20:29 +0800

    12.24

---
+++ springboot_03_read_data/src/main/java/com/yxy/Application03.java
@@ -0,0 +1,13 @@
+package com.yxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application03 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application03.class, args);
+    }
+
+}
--- springboot_03_read_data/src/main/java/com/yxy/controller/BookController.java
+++ springboot_03_read_data/src/main/java/com/yxy/controller/BookController.java
@@ -1,5 +1,9 @@
 package com.yxy.controller;
 
+import com.yxy.domain.Enterprise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    // 用此种方法，读取yml文件中的属性
+    // 方式一
+    @Value("${lesson}")
+    private String lesson;
+
+    @Value("${server.port}")
+    private Integer prot;
+
+    @Value("${enterprise.subject[0]}")
+    private String subject;
+
+    // 方式二
+    @Autowired
+    private Environment environment;
+
+    // 方式三
+    @Autowired
+    private Enterprise enterprise;
+
+
     @GetMapping("/{id}")
     public String book(@PathVariable String id){
         System.out.println("id=>" + id);
+        System.out.println(lesson);
+        System.out.println(prot);
+        System.out.println(subject);
+        System.out.println("--------------------");
+        System.out.println(environment.getProperty("lesson"));
+        System.out.println(environment.getProperty("server.port"));
+        System.out.println(environment.getProperty("enterprise.subject[1]"));
+        System.out.println("--------------------");
+        System.out.println(enterprise.getName());
+        System.out.println(enterprise.getAge());
+        System.out.println(enterprise.getSubject()[0]);
         return "book controller";
     }
 }
